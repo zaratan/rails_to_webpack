e5768b10364bae8015982798451612b1af0b2f5f
Denis Zaratan Pasin <denis@pasin.fr>
2019-01-19 19:08:51 +0900

    Clean

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc).includes(:author)
     respond_to do |format|
-      format.html do
-        @new_post = Post.new
-      end
+      format.html {}
       format.json do
         render json: @posts
       end
@@ -20,42 +18,21 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create!(create_params.merge(author: current_user))
-    respond_to do |format|
-      format.html do
-        redirect_to root_path
-      end
-      format.json do
-        render json: post
-      end
-    end
+    render json: post
   end
 
   def destroy
     post = Post.find(params[:id])
     authorize post
     post.destroy
-    respond_to do |format|
-      format.html do
-        redirect_to root_path
-      end
-      format.json do
-        render json: post
-      end
-    end
+    render json: post
   end
 
   def update
     post = Post.find(params[:id])
     authorize post
     post.update!(update_params)
-    respond_to do |format|
-      format.html do
-        redirect_to root_path
-      end
-      format.json do
-        render json: post
-      end
-    end
+    render json: post
   end
 
   private
@@ -66,38 +43,14 @@ class PostsController < ApplicationController
   alias_method :update_params, :create_params
 
   def not_authorized_error
-    respond_to do |format|
-      format.html do
-        flash[:alert] = "This is not your post"
-        redirect_to(root_path)
-      end
-      format.json do
-        render json: { errors: ["This is not your post"] }
-      end
-    end
+    render json: { errors: ["This is not your post"] }
   end
 
   def record_not_found
-    respond_to do |format|
-      format.html do
-        flash[:alert] = "The post doesn't exist"
-        redirect_to(root_path)
-      end
-      format.json do
-        render json: { errors: ["The post doesn't exist"] }
-      end
-    end
+    render json: { errors: ["The post doesn't exist"] }
   end
 
   def rescue_bad_params(exception)
-    respond_to do |format|
-      format.html do
-        flash[:alert] = exception.record.errors.full_messages.join(", ")
-        redirect_to(root_path)
-      end
-      format.json do
-        render json: { errors: exception.record.errors.full_messages }
-      end
-    end
+    render json: { errors: exception.record.errors.full_messages }
   end
 end
